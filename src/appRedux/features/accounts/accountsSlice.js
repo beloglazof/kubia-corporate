434ed0b3aee40e04fc335031b4224dc85097db3d
Vyacheslav Beloglazov <beloglazov@quancy.com.sg>
2019-10-29 09:34:19 +0500

    Fix first load

--- src/appRedux/features/accounts/accountsSlice.js
+++ src/appRedux/features/accounts/accountsSlice.js
@@ -1,17 +1,18 @@
-import { createSlice } from 'redux-starter-kit';
+import { createSlice } from "redux-starter-kit";
 
-const storageAccounts = localStorage.getItem('accounts')
+const storageAccounts = localStorage.getItem("accounts");
 
 const accountsSlice = createSlice({
-  name: 'accounts',
-  initialState: storageAccounts ? JSON.parse(storageAccounts) : [] ,
+  name: "accounts",
+  initialState: storageAccounts ? JSON.parse(storageAccounts) : [],
   reducers: {
     setAccounts(state, action) {
-      state = action.payload
+      const accounts = action.payload;
+      accounts.forEach((account, index) => (state[index] = account));
     }
   }
-})
+});
 
-export const {setAccounts} = accountsSlice.actions;
+export const { setAccounts } = accountsSlice.actions;
 
 export default accountsSlice.reducer;
