1e0acf68a95b5808b4d692f9ea4122f27f6c69e0
Vyacheslav Beloglazov <beloglazov@quancy.com.sg>
2019-12-17 11:50:23 +0500

    requisites modal and button

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,18 +1,47 @@
-import { Button, Card as UiCard, Col, List, Row, Collapse } from 'antd';
-import React from 'react';
+import {
+  Button,
+  Card as UiCard,
+  Col,
+  List,
+  Row,
+  Collapse,
+  Divider,
+  Modal,
+  Descriptions
+} from 'antd';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link, useHistory } from 'react-router-dom';
 import styles from './account.module.css';
 import LastTransactions from '../Transactions/LastTransactions';
 import PropTypes from 'prop-types';
+import { startCase } from 'lodash';
+import { useModal } from 'sunflower-antd';
+
+const renderField = ([name, value]) => {
+  const label = startCase(name);
+  return (
+    <Descriptions.Item label={label} key={name}>
+      <span style={{ fontWeight: 'bold' }}>{value}</span>
+    </Descriptions.Item>
+  );
+};
+
+const renderFields = (fieldsObj = {}) => {
+  const fields = Object.entries(fieldsObj);
+  const mapped = fields.map(renderField);
+  return mapped;
+};
 
 const Account = ({ account }) => {
   const user = useSelector(({ user }) => user);
   const { first_name, last_name } = user;
   const name = first_name && last_name && `${first_name} ${last_name}`;
 
-  const { number, amount, currency_info, id } = account;
-
+  const { number, amount, currency_info, id, bank_deposit } = account;
+  const { modalProps, show } = useModal({
+    defaultVisible: false
+  });
   return (
     <List.Item key={id}>
       <UiCard title="Account info" bodyStyle={{ paddingBottom: '10px' }}>
@@ -30,9 +59,25 @@ const Account = ({ account }) => {
         </Row>
         <Row className={styles.row}>
           <Col span={8}>
-            <Button type="primary">
-              <Link to="/new-payment">Make Payment</Link>
-            </Button>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                flexDirection: 'row'
+              }}
+            >
+              <Button type="primary">
+                <Link to="/new-payment">Make Payment</Link>
+              </Button>
+              <Button type="primary" onClick={() => show()}>
+                Requisites
+              </Button>
+              <Modal title="Account requisites" footer={null} {...modalProps}>
+                <Descriptions bordered column={2} layout="vertical">
+                  {renderFields(bank_deposit)}
+                </Descriptions>
+              </Modal>
+            </div>
           </Col>
         </Row>
         <Row className={styles.row}>
