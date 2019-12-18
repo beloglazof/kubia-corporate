4fa81db98d33f389b1274419e385e24acbbcf5f1
Vyacheslav Beloglazov <beloglazov@quancy.com.sg>
2019-12-18 10:46:33 +0500

    handle logo size

--- src/components/MainLogo.jsx
+++ src/components/MainLogo.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../assets/images/main-logo.png';
+import logo from '../assets/images/main-logo.webp';
 
 const MainLogo = () => {
   return <img src={logo} alt="Kubia" />
